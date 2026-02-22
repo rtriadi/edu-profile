6b4241cf72f4b6d3922d9aeeb7f47324cdd16373
Rahmat Triadi <rtriadi01@gmail.com>
2026-02-22 17:27:48 +0800

    Revert "fix: simplify prisma client + remove DB calls from ThemeStyles"
    This reverts commit fa3d99468bfdbbcfa41d72207e6f7506b32a03e9.

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -4,13 +4,7 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-function getClient() {
-  return new PrismaClient({
-    log: process.env.NODE_ENV === "production" ? ["error"] : ["error", "warn"],
-  });
-}
-
-export const prisma = globalForPrisma.prisma ?? getClient();
+export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
